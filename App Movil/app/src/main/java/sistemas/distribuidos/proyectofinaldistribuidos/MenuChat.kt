43c83c7f06b2f2cab7e3aa1972a90186c12fed46
JesusGlezAcosta <71987633+JesusGlezAcosta@users.noreply.github.com>
2022-05-17 18:14:14 -0800

    Pantallas

---
+++ App Movil/app/src/main/java/sistemas/distribuidos/proyectofinaldistribuidos/MenuChat.kt
@@ -0,0 +1,11 @@
+package sistemas.distribuidos.proyectofinaldistribuidos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuChat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_chat)
+    }
+}
